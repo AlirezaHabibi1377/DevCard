a7afc8d376a30ae0fbf01527e4221dbc9ccc8421
ALIREZA <mo.alireza_77@yahoo.com>
2022-07-30 11:33:37 -0500

    projects section from index page_Partial view

---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+    }
+}
