f8a9c3495bf82bd82b8fc69a33276b89ce2dd212
ALIREZA <mo.alireza_77@yahoo.com>
2022-07-30 10:57:15 -0500

    Index & Contact pages added

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,21 +1,15 @@
 ﻿using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -23,10 +17,11 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public ActionResult Contact()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
